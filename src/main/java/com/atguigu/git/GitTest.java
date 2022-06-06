b38e84d0d1f0bfdb6e3a6918cb5c5147138843b0
yzw <yezw111@163.com>
2022-06-06 23:03:17 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author yezw
+ * @create 2022-06-06 22:57
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
