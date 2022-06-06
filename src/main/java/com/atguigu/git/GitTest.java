87f1a4528972814dba2e8dcc03ac57d9af67c69a
yzw <yezw111@163.com>
2022-06-06 23:24:25 +0800

    push commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,5 +10,7 @@ public class GitTest {
         System.out.println("hello git2");
         System.out.println("hello git2");
         System.out.println("hello git2");
+        System.out.println("hello git2");
+        System.out.println("hello git2");
     }
 }
